c9fbbc87eee6bfc324433ef6400b9d19265faccc
Himanshu4782652 <himanshusriwastava111@gmail.com>
2024-08-28 08:46:03 +0530

    adding using of form data and json data in flask

---
+++ Request_object_demo/script_2.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def take_data():
+   return render_template('take_data_2.html')
+
+@app.route("/fetchData", methods=['post'])
+def fetch_data():
+   # print(request.form)
+   # return "Request form is printed"
+   return "<h2>The language is {} and the framework is {}".format(request.form.get('txtlanguage'),request.form.get('txtframework'))
+
+if __name__=="__main__":
+   app.run(debug=True)
---
+++ Request_object_demo/script_3.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/makeJson')
+def make_json():
+    person = {
+        "name": "Himanshu",
+        "language": "python",
+        "framework": ["flask", "django", "bottle"],
+    }
+    res = requests.post("http://127.0.0.1:5000/processJson", json=person)
+    return res.text
+
+
+@app.route('/processJson', methods=['post'])
+def process_json():
+    if request.is_json:
+      #   return "It has json data"
+      return request.json
+    else:
+        return "It does'nt have json data"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
