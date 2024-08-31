c5a2cfd64ea0e62ac0c32a11994374f3c646c82b
Himanshu4782652 <himanshusriwastava111@gmail.com>
2024-08-31 23:28:32 +0530

    adding session handling with cookies

--- Request_object_demo/script_1.py
+++ Request_object_demo/script_1.py
@@ -2,15 +2,18 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/takeData')
+
+@app.route("/takeData")
 def take_data():
-   return render_template('take_data.html')
+    return render_template("take_data.html")
 
-@app.route('/queryDemo')
+
+@app.route("/queryDemo")
 def query_demo():
-   # return render_template('query_demo.html')
-   # print(request.args)
-   return "Language Entered is {}".format(request.args.get('txtlanguage'))
+    # return render_template('query_demo.html')
+    # print(request.args)
+    return "Language Entered is {}".format(request.args.get("txtlanguage"))
 
-if __name__=="__main__":
-   app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Request_object_demo/script_2.py
+++ Request_object_demo/script_2.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def take_data():
-   return render_template('take_data_2.html')
+    return render_template("take_data_2.html")
 
-@app.route("/fetchData", methods=['post'])
+
+@app.route("/fetchData", methods=["post"])
 def fetch_data():
-   # print(request.form)
-   # return "Request form is printed"
-   return "<h2>The language is {} and the framework is {}".format(request.form.get('txtlanguage'),request.form.get('txtframework'))
+    # print(request.form)
+    # return "Request form is printed"
+    return "<h2>The language is {} and the framework is {}".format(
+        request.form.get("txtlanguage"), request.form.get("txtframework")
+    )
 
-if __name__=="__main__":
-   app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
