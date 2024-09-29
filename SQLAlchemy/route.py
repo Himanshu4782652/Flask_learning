e07f4e782747ac97f015b8e4838e0f9b203a2aa4
Himanshu4782652 <himanshusriwastava111@gmail.com>
2024-09-30 01:12:19 +0530

    SQLAlchemy

--- SQLAlchemy/route.py
+++ SQLAlchemy/route.py
@@ -14,7 +14,39 @@ class student(db.Model):
     city = db.Column(db.String(50))
     pin = db.Column(db.String(10))
 
+    def __init__(self, name, addr, city, pin) -> None:
+        super().__init__()
+        self.name = name
+        self.addr = addr
+        self.city = city
+        self.pin = pin
+
+
+@app.route("/insert")
+def insert_student():
+    return render_template("insert_student.html")
+
+
+@app.route("/queryStudent")
+def queryStudent():
+ all_student = student.query.all()
+ return render_template("/listStudent.html", data=all_student)
+
+@app.route("/submitStudent", methods=["POST"])
+def submit_student():
+    name = request.form.get("txtname")
+    address = request.form.get("txtaddr")
+    city = request.form.get("txtcity")
+    pin = request.form.get("txtpin")
+    stud = student(name, address, city, pin)
+
+    db.session.add(stud)
+    db.session.commit()
+    msg = "Record Inseted Successfully"
+    return render_template("success.html", msg=msg)
+
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():  # Push application context
+        db.create_all()  # Create tables in the database
     app.run(debug=True)
